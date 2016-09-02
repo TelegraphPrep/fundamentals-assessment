46c4a62623b098819a528aa4b268b04c1db426d6
phedoraj <phedoraj@users.noreply.github.com>
2016-09-01 23:50:57 -0400

    Completed fundamentals assessment

--- arrays_assessment/arraysAssessment.js
+++ arrays_assessment/arraysAssessment.js
@@ -1,4 +1,8 @@
 var myArray = [ 'I',  'am',  'currently',  'creating',  'your',  'self',  'assessments',  'in',  'a',  'cafe',  'that',  'has',  'a',  'jazz',  'school',  'adjacent',  'to',  'it',  'in',  'Berkeley,',  'California.',  'These',  'words', 'are',  'merely',  'created',  'to',  'supply',  'myArray',  'values,',  'and',  'altogether',  'do',  'not',  'have',  'any',  'greater',  'importance.',  'Have',  'a',  'nice',  'day.' ];
 
-//YOUR CODE HERE
-
+var lastWord = myArray[myArray.length-1];
+console.log(myArray.length);
+console.log(myArray[22]);
+for(var i= 0; i< myArray.length; i++);
+console.log(myArray);
+myArray.push("Goodbye");
